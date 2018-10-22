1f481cbffce6ce368ac63ab1ef929812a709e900
John Chen <shiguangchen1987@gmail.com>
2018-10-22 09:35:33 -0700

    Add files via upload

---
+++ resolvers/user.js
@@ -0,0 +1,22 @@
+import bcrypt from 'bcrypt';
+
+export default {
+    Query: {
+        getUser: (parent, { id }, { models }) => models.User.findOne({where: { id }}),
+        allUsers: (parent, arg, {models}) => models.User.findAll(),
+    },
+    Mutation: {
+        register: async (parent, {password, ...otherArgs}, {models}) => {
+          try {
+            const hashedPassword = await bcrypt.hash(password, 12);
+            await models.User.create({ ...otherArgs, password: hashedPassword });
+            return true;
+          } catch (err){
+              return false;
+          }
+        },
+    },
+    // Mutation: {
+    //     createUser: (parent, args, { models }) => models.User.create(args),
+    // },
+};
